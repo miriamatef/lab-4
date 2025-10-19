f08cc1d3e318bddd5c87ba3cec01fb81685717c4
mariamelshamy-20 <marybasemad@gmail.com>
2025-10-20 01:25:12 +0300

    Refactor Product class and update package name

--- Product.java
+++ Product.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package lab4_final;
+package lab4;
 
 /**
  *
@@ -91,3 +91,4 @@ public class Product {
     return lineRepresentation() + System.lineSeparator();
 }
 }
+
